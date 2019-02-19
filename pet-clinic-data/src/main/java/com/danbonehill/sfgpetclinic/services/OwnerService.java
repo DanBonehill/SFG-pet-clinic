70598b4dc0b67b28f4924de39ab90c9462142b82
DanBonehill <DanBonehill@me.com>
2019-02-19 13:20:38 +0000

    Implement Interfaces for services. Closes Issue #5

---
+++ pet-clinic-data/src/main/java/com/danbonehill/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package com.danbonehill.sfgpetclinic.services;
+
+import com.danbonehill.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/danbonehill/sfgpetclinic/services/VetService.java
@@ -0,0 +1,15 @@
+package com.danbonehill.sfgpetclinic.services;
+
+import com.danbonehill.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+
+}
