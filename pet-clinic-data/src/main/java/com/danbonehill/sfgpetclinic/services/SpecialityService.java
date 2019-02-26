99fcb9bfc9fb868f55226ba8ed835d63594290a3
DanBonehill <DanBonehill@me.com>
2019-02-26 17:20:28 +0000

    Create SpecialityService and Map impl, closes #30.

---
+++ pet-clinic-data/src/main/java/com/danbonehill/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.danbonehill.sfgpetclinic.services;
+
+import com.danbonehill.sfgpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
