11de5bb6d0970e2be6586563cca756cb8e505415
DanBonehill <DanBonehill@me.com>
2019-03-01 11:11:41 +0000

    Create Spring Data JPA Service for Visits, closes #46.

---
+++ pet-clinic-data/src/main/java/com/danbonehill/sfgpetclinic/services/springdatajpa/VisitSDService.java
@@ -0,0 +1,48 @@
+package com.danbonehill.sfgpetclinic.services.springdatajpa;
+
+import com.danbonehill.sfgpetclinic.model.Visit;
+import com.danbonehill.sfgpetclinic.repositories.VisitRepository;
+import com.danbonehill.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDService implements VisitService {
+
+    private VisitRepository visitRepository;
+
+    public VisitSDService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
--- pet-clinic-web/src/main/java/com/danbonehill/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/danbonehill/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.danbonehill.sfgpetclinic.bootstrap;
 
 import com.danbonehill.sfgpetclinic.model.*;
-import com.danbonehill.sfgpetclinic.services.OwnerService;
-import com.danbonehill.sfgpetclinic.services.PetTypeService;
-import com.danbonehill.sfgpetclinic.services.SpecialityService;
-import com.danbonehill.sfgpetclinic.services.VetService;
+import com.danbonehill.sfgpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+                      SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -74,6 +74,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasPet);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasPet);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners and Pets....");
 
