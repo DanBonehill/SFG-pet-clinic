60dada9b2d2c613b8b8d59042702fb2bda4f1a0f
DanBonehill <DanBonehill@me.com>
2019-03-01 11:42:58 +0000

    Update Profiles to use Spring Data JPA, closes #47.

--- pet-clinic-data/src/main/java/com/danbonehill/sfgpetclinic/services/map/PetTypeServiceMap.java
+++ pet-clinic-data/src/main/java/com/danbonehill/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -2,11 +2,13 @@ package com.danbonehill.sfgpetclinic.services.map;
 
 import com.danbonehill.sfgpetclinic.model.PetType;
 import com.danbonehill.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
 
     @Override
--- pet-clinic-data/src/main/java/com/danbonehill/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/danbonehill/sfgpetclinic/services/map/VetServiceMap.java
@@ -4,11 +4,13 @@ import com.danbonehill.sfgpetclinic.model.Speciality;
 import com.danbonehill.sfgpetclinic.model.Vet;
 import com.danbonehill.sfgpetclinic.services.SpecialityService;
 import com.danbonehill.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     private final SpecialityService specialityService;
