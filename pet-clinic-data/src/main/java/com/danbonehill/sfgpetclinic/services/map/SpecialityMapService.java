92b0ea0da75073bf989dcca235b3b89b6e122ef6
DanBonehill <DanBonehill@me.com>
2019-03-04 14:28:08 +0000

    Renamed Map Services

--- pet-clinic-data/src/main/java/com/danbonehill/sfgpetclinic/services/map/SpecialityMapService.java
+++ pet-clinic-data/src/main/java/com/danbonehill/sfgpetclinic/services/map/SpecialityMapService.java
@@ -1,7 +1,7 @@
 package com.danbonehill.sfgpetclinic.services.map;
 
-import com.danbonehill.sfgpetclinic.model.PetType;
-import com.danbonehill.sfgpetclinic.services.PetTypeService;
+import com.danbonehill.sfgpetclinic.model.Speciality;
+import com.danbonehill.sfgpetclinic.services.SpecialityService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -9,25 +9,25 @@ import java.util.Set;
 
 @Service
 @Profile({"default", "map"})
-public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
 
     @Override
-    public Set<PetType> findAll() {
+    public Set<Speciality> findAll() {
         return super.findAll();
     }
 
     @Override
-    public PetType findById(Long id) {
+    public Speciality findById(Long id) {
         return super.findById(id);
     }
 
     @Override
-    public PetType save(PetType object) {
+    public Speciality save(Speciality object) {
         return super.save(object);
     }
 
     @Override
-    public void delete(PetType object) {
+    public void delete(Speciality object) {
         super.delete(object);
     }
 
