acbb68636f577034f9f5349bf24196619396075d
DanBonehill <DanBonehill@me.com>
2019-03-11 14:22:29 +0000

    Implement Find Owner, closes #53

--- pet-clinic-data/src/main/java/com/danbonehill/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/danbonehill/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package com.danbonehill.sfgpetclinic.services;
 
 import com.danbonehill.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
